938d2b518ffcec30e649b20e3565541022c7f78c
DevOps Think <devopsthink@gmail.com>
2020-05-24 15:29:49 +0000

    updated table name

--- db/mysql-init.sql
+++ db/mysql-init.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE `sample`;
+# CREATE DATABASE `sample`;
 
-use sample;
-CREATE TABLE `user1` (
+ use sample;
+ CREATE TABLE `user` (
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(15) NOT NULL,
   `regdate` tinyint NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
