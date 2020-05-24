b19f1b67d334ec4371810720a361834d3f845948
DevOps Think <devopsthink@gmail.com>
2020-05-24 14:35:57 +0000

    db directory and db script added

---
+++ db/mysql-init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `sample`;
+
+use sample;
+CREATE TABLE `user1` (
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `regdate` tinyint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
